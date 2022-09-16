807066338f4ec65f2e748320209f9d7e08713264
Daniel Gonzalez <danielgnzch@gmail.com>
2022-09-16 09:51:38 +0200

    Closed #46 Back button is shown in ErrorPage when not is logged in

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,16 +1,21 @@
+import { useContext } from "react";
 import React from "react";
 import Navbar from "../components/Navbar";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 export default function ErrorPage() {
+  const { isLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
     <div>
       <Navbar />
       <p>404 - Page not found</p>
-      <button className="btn-style3" onClick={() => navigate(`/`)}>
-        {String.fromCharCode(8592)} Back
-      </button>
+      {!isLoggedIn && (
+        <button className="btn-style3" onClick={() => navigate(`/`)}>
+          {String.fromCharCode(8592)} Back
+        </button>
+      )}
     </div>
   );
 }
