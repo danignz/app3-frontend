9061a87109ebd37c0885471b36bb9b3fd88406f1
Daniel Gonzalez <danielgnzch@gmail.com>
2022-09-24 19:32:15 +0200

    Closed #84 Bug in portfolio view, wrong query in filter method

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -10,8 +10,7 @@ export default function Portfolio() {
   const [projectsTL, setProjectsTL] = useState([]);
   const [projectsColaborator, setProjectsColaborator] = useState([]);
   const storedToken = localStorage.getItem("authToken");
-  const [user, setUser] = useState({ profession: "" });
-  const [enumValues, setEnumValues] = useState({ profession: [] });
+  const [user, setUser] = useState({});
   const { id } = useParams();
   document.title = `Iron Co-Workers | Portfolio`;
 
@@ -22,7 +21,6 @@ export default function Portfolio() {
           `${process.env.REACT_APP_API_URL}/users/${id}`,
           { headers: { Authorization: `Bearer ${storedToken}` } }
         );
-
         setUser(response.data.data);
       } catch (error) {
         console.error(error);
@@ -36,21 +34,6 @@ export default function Portfolio() {
     const getData = async () => {
       try {
         const response = await axios.get(
-          `${process.env.REACT_APP_API_URL}/users/enum-values`
-        );
-        setEnumValues(response.data.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getData();
-    // eslint-disable-next-line
-  }, []);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.get(
           `${process.env.REACT_APP_API_URL}/projects`,
           { headers: { Authorization: `Bearer ${storedToken}` } }
         );
@@ -59,17 +42,12 @@ export default function Portfolio() {
         );
         setProjectsTL(myProjectsTL);
 
-        const indexCollaborator = enumValues.profession.indexOf(
-          user.profession
-        );
-        const myProjectsColaborator = response.data.data.filter(
-          (project) =>
-            //first render enumValues is empty amb indexOf return -1, skip first render
-            indexCollaborator !== -1 &&
-            project.collaborators[indexCollaborator].users.some(
-              (user) => user._id === id
-            ) &&
-            project.status === "Closed"
+        const myProjectsColaborator = response.data.data.filter((project) =>
+          project.collaborators.some((rol) =>
+            rol.users.some(
+              (user) => user._id === id && project.status === "Closed"
+            )
+          )
         );
         setProjectsColaborator(myProjectsColaborator);
       } catch (error) {
@@ -78,7 +56,7 @@ export default function Portfolio() {
     };
     getData();
     // eslint-disable-next-line
-  }, [id, enumValues, user]);
+  }, [id, user]);
 
   return (
     <div className="backgroundcolor">
@@ -95,7 +73,9 @@ export default function Portfolio() {
           </div>
 
           {projectsColaborator.length === 0 && (
-            <p style={{ color: "black", fontSize: 20, paddingTop: 25 }}> ⚠️ There is not projects closed as a collaborator.
+            <p style={{ color: "black", fontSize: 20, paddingTop: 25 }}>
+              {" "}
+              ⚠️ There is not projects closed as a collaborator.
             </p>
           )}
           {projectsColaborator.length !== 0 &&
@@ -115,7 +95,9 @@ export default function Portfolio() {
           </div>
 
           {projectsTL.length === 0 && (
-            <p style={{ color: "black", fontSize: 20, paddingTop: 25 }}> ⚠️ There is not projects closed as a team lead.
+            <p style={{ color: "black", fontSize: 20, paddingTop: 25 }}>
+              {" "}
+              ⚠️ There is not projects closed as a team lead.
             </p>
           )}
           {projectsTL.length !== 0 &&
